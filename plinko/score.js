787a9feaf22676cd90354b6a93e9122f0210519d
Arthur Sandro <arthurcs426@gmail.com>
2020-12-20 15:25:49 -0800

    made some changes to test some dummy results

--- plinko/score.js
+++ plinko/score.js
@@ -1,12 +1,16 @@
-let outputs = [];
+let outputs = [
+  [10, 0.5, 16, 1],
+  [200, 0.5, 16, 4],
+  [350, 0.5, 16, 4],
+  [600, 0.5, 16, 5],
+];
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  console.log(outputs);
+  // console.log(outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
 }
-
